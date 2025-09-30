6f83742e5451fadfc6a43bb4d8f35c44daa20ffb
Omar Farouk EL FILALI <omarelfiie2007@gmail.com>
2025-09-30 18:58:37 +0100

    finale project first push

---
+++ Finale.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    printf("======================================================\n");
+    printf("||              ZZZZZZ   OOOOO    OOOOO             ||\n");
+    printf("||                 Z    O     O  O     O            ||\n");
+    printf("||                Z     O     O  O     O            ||\n");
+    printf("||               Z      O     O  O     O            ||\n");
+    printf("||              ZZZZZZ   OOOOO    OOOOO             ||\n");
+    printf("||                                                  ||\n");
+    printf("||                                                  ||\n");
+    printf("||                  ZOO MANAGER APP                 ||\n");
+    printf("||                                                  ||\n");
+    printf("||                     1.[Add]                      ||\n");
+    printf("||                     2.[Show]                     ||\n");
+    printf("||                     3.[Modify]                   ||\n");
+    printf("||                     4.[Delete]                   ||\n");
+    printf("||                                                  ||\n");
+    printf("======================================================\n");
+
+    return 0;
+}
